9783ada60bd8dba60bc1f4785c5a88b8bef443a3
eshamster <hamgoostar@gmail.com>
2015-03-30 02:32:30 +0900

    - add ArrowMover script (now to rotate arrow in air) - add simplified sighter - some refactor

---
+++ Assets/ArrowShot/ArrowMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMover : MonoBehaviour
+{
+    public float rotateThreshold = 0.3f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.GetComponent<Rigidbody>().velocity.magnitude > rotateThreshold)
+        {
+            gameObject.transform.up = Vector3.Slerp(gameObject.transform.up, gameObject.GetComponent<Rigidbody>().velocity.normalized, Time.deltaTime * 2);
+        }
+    }
+}
--- Assets/ArrowShot/ShotProtoArrow.cs
+++ Assets/ArrowShot/ShotProtoArrow.cs
@@ -5,6 +5,8 @@ public class ShotProtoArrow : MonoBehaviour
 {
     public Transform arrow;
     public float power;
+    public Vector3 targetPnt = new Vector3(0, 0, 1);
+    public Vector3 startPnt;
 
 	// Use this for initialization
 	void Start () 
@@ -19,8 +21,12 @@ public class ShotProtoArrow : MonoBehaviour
         {
             var parent = gameObject.transform;
             var parentEuler = parent.rotation.eulerAngles;
-            var obj = (Transform)Instantiate(arrow, parent.position + Quaternion.Euler(0, parentEuler.y,0) * new Vector3(0.3f, 1, 0) , Quaternion.Euler(90, 0, -parentEuler.y));
-            obj.GetComponent<Rigidbody>().AddForce(Quaternion.Euler(0, parentEuler.y,0) * new Vector3(0, 0, power), ForceMode.Impulse);
+            var shotAngle = 
+                Quaternion.Euler(0, parentEuler.y, 0) * 
+                Quaternion.Euler(Quaternion.LookRotation(targetPnt - startPnt).eulerAngles + new Vector3(90, 0, 0));
+
+            var obj = (Transform)Instantiate(arrow, parent.position + Quaternion.Euler(0, parentEuler.y, 0) * startPnt, shotAngle);
+            obj.GetComponent<Rigidbody>().AddForce(Quaternion.Euler(0, parentEuler.y, 0) * (targetPnt - startPnt).normalized * power, ForceMode.Impulse);
         }
 	}
 }
